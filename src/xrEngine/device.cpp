417181faceed95e9a3c4988a859b9ff63c75c7de
xrMorrazzzz <123486080+morrazzzz@users.noreply.github.com>
2023-06-29 08:40:30 +0300

    fix vid_restart

--- src/xrEngine/device.cpp
+++ src/xrEngine/device.cpp
@@ -119,6 +119,10 @@ void CRenderDevice::End(void)
         ::Sound->set_master_volume(0.f);
         dwPrecacheFrame--;
         //. pApp->load_draw_internal ();
+
+        if (!load_screen_renderer.b_registered)
+            m_pRender->ClearTarget();
+
         if (0 == dwPrecacheFrame)
         {
 
