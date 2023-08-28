34f74d973a809630080599a60430c0f7758a879a
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2023-08-28 10:03:00 +0300

    Новый алгоритм psHUD_FOV

--- src/Layers/xrRenderPC_R2/r2_rendertarget_accum_point.cpp
+++ src/Layers/xrRenderPC_R2/r2_rendertarget_accum_point.cpp
@@ -9,22 +9,22 @@ void CRenderTarget::accum_point		(light* L)
 	if (!shader)	shader			= s_accum_point;
 
 
-	Fmatrix Pold=Fidentity;
-	Fmatrix FTold=Fidentity;
+	Fmatrix Pold = Fidentity;
+	Fmatrix FTold = Fidentity;
 
 	if (L->flags.bHudMode)
 	{
-		extern ENGINE_API float		psHUD_FOV;
-		Pold				= Device.mProject;
-		FTold				= Device.mFullTransform;
-		Device.mProject.build_projection(
-			deg2rad(psHUD_FOV*Device.fFOV /* *Device.fASPECT*/ ), 
-			Device.fASPECT, VIEWPORT_NEAR, 
-			g_pGamePersistent->Environment().CurrentEnv->far_plane);
+		extern ENGINE_API float psHUD_FOV;
 
-		Device.mFullTransform.mul	(Device.mProject, Device.mView);
-		RCache.set_xform_project	(Device.mProject);
-		RImplementation.rmNear		();
+		Pold = Device.mProject;
+		FTold = Device.mFullTransform;
+
+		Device.mProject.build_projection(deg2rad(psHUD_FOV * 84.f), Device.fASPECT, VIEWPORT_NEAR, g_pGamePersistent->Environment().CurrentEnv->far_plane);
+
+		Device.mFullTransform.mul(Device.mProject, Device.mView);
+
+		RCache.set_xform_project(Device.mProject);
+		RImplementation.rmNear();
 	}
 
 	// Common
