b44d0f30125e992ae3ed8d984d98776e682c9f7f
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2024-03-26 12:06:46 +0300

    Fix UIWpnParams

--- src/xrGame/ui/UIWpnParams.cpp
+++ src/xrGame/ui/UIWpnParams.cpp
@@ -194,13 +194,13 @@ void CUIWpnParams::SetInfo( CInventoryItem* slot_wpn, CInventoryItem& cur_wpn )
 		Frect				tex_rect;
 		tex_rect.x1			= float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_x") * INV_GRID_WIDTH);
 		tex_rect.y1			= float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_y") * INV_GRID_HEIGHT);
-		tex_rect.x2			= float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_width") * INV_GRID_WIDTH )/2.0;
-		tex_rect.y2			= float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_height") * INV_GRID_HEIGHT)/2.0;
+		tex_rect.x2			= float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_width") * INV_GRID_WIDTH );
+		tex_rect.y2			= float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_height") * INV_GRID_HEIGHT);
 		tex_rect.rb.add		(tex_rect.lt);
 		m_stAmmoType1.SetTextureRect(tex_rect);
 		m_stAmmoType1.TextureOn();
 		m_stAmmoType1.SetStretchTexture(true);
-		m_stAmmoType1.SetWndSize(Fvector2().set((tex_rect.x2-tex_rect.x1)*UI().get_current_kx(), tex_rect.y2-tex_rect.y1));
+		m_stAmmoType1.SetWndSize(Fvector2().set((tex_rect.x2 - tex_rect.x1) * UI().get_current_kx() / 2, (tex_rect.y2 - tex_rect.y1) / 2));
 
 		m_stAmmoType2.SetShader(InventoryUtilities::GetEquipmentIconsShader());
 		if(ammo_types.size()==1)
@@ -218,7 +218,7 @@ void CUIWpnParams::SetInfo( CInventoryItem* slot_wpn, CInventoryItem& cur_wpn )
 		m_stAmmoType2.SetTextureRect(tex_rect);
 		m_stAmmoType2.TextureOn();
 		m_stAmmoType2.SetStretchTexture(true);
-		m_stAmmoType2.SetWndSize(Fvector2().set((tex_rect.x2-tex_rect.x1)*UI().get_current_kx(), tex_rect.y2-tex_rect.y1));
+		m_stAmmoType2.SetWndSize(Fvector2().set((tex_rect.x2 - tex_rect.x1) * UI().get_current_kx() / 2, (tex_rect.y2 - tex_rect.y1) / 2));
 	}
 }
 
