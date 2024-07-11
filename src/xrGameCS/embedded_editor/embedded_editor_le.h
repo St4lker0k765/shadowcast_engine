10301c9778f62771fa28e6c1701bba336be085d6
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2024-07-11 12:28:27 +0300

    Удалены Embedded Editor for ImGui Files

--- src/xrGameCS/embedded_editor/embedded_editor_le.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void showLeEditor(bool& show);
--- src/xrGameCS/embedded_editor/embedded_editor_prop.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class CObject;
-void ShowPropEditor(bool& show);
-void SetPropObject(CObject* obj);
-void ShowLuaBinder(bool& show);
-
-extern bool show_lua_binder;
