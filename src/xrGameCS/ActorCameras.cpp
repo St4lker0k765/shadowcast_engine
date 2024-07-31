0fab10316a97a6c29511a3eb303f849ee67a4268
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2024-07-31 22:57:53 +0300

    [ЧН] Рабочая коллизия худа

--- src/xrGameCS/ActorCameras.cpp
+++ src/xrGameCS/ActorCameras.cpp
@@ -25,6 +25,9 @@
 extern BOOL dbg_draw_camera_collision;
 void	collide_camera( CCameraBase & camera, float _viewport_near  );
 
+ENGINE_API extern float psHUD_FOV; //--#SM+#--
+ENGINE_API extern float psHUD_FOV_def; //--#SM+#--
+
 void CActor::cam_Set	(EActorCameras style)
 {
 	CCameraBase* old_cam = cam_Active();
@@ -273,6 +276,17 @@ void	CActor::cam_Lookout	( const Fmatrix &xform, float camera_height )
 void CActor::cam_Update(float dt, float fFOV)
 {
 	if(m_holder)		return;
+
+	// HUD FOV Update --#SM+#--
+	if (this == Level().CurrentControlEntity())
+	{
+		CWeapon* pWeapon = smart_cast<CWeapon*>(this->inventory().ActiveItem());
+		if (eacFirstEye == cam_active && pWeapon)
+			psHUD_FOV = pWeapon->GetHudFov();
+		else
+			psHUD_FOV = psHUD_FOV_def;
+	}
+	//--#SM+#--
 
 	if( (mstate_real & mcClimb) && (cam_active!=eacFreeLook) )
 		camUpdateLadder(dt);
--- src/xrGameCS/console_commands.cpp
+++ src/xrGameCS/console_commands.cpp
@@ -66,7 +66,7 @@ extern	u64		g_qwStartGameTime;
 extern	u64		g_qwEStartGameTime;
 
 ENGINE_API
-extern	float	psHUD_FOV;
+extern  float	psHUD_FOV_def; //--#SM+#-- Команда для смены FOV теперь меняет это [it's now used in hud_fov]
 extern	float	psSqueezeVelocity;
 extern	int		psLUA_GCSTEP;
 
@@ -1736,7 +1736,7 @@ void CCC_RegisterCommands()
 	CMD3(CCC_Mask,				"hud_crosshair",		&psHUD_Flags,	HUD_CROSSHAIR);
 	CMD3(CCC_Mask,				"hud_crosshair_dist",	&psHUD_Flags,	HUD_CROSSHAIR_DIST);
 
-	CMD4(CCC_Float,				"hud_fov",				&psHUD_FOV,		0.1f,	1.0f);
+	CMD4(CCC_Float,				"g_hud_fov",			&psHUD_FOV_def,	0.1f,	1.0f) // [FFT++]: изменено в коллизии шокера
 	CMD4(CCC_Float,				"fov",					&g_fov,			5.0f,	180.0f);
 
 	CMD1(CCC_Spawn_to_inventory, "g_spawn_to_inventory")
