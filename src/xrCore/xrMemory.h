0beeb0bd530c544ce903c08bd2643e2c1dd6daa0
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2024-08-12 20:08:45 +0300

    корректировка проектов и убрал ненужные дефайны

--- src/xrCore/xrMemory.h
+++ src/xrCore/xrMemory.h
@@ -83,13 +83,6 @@ public:
 
 extern XRCORE_API xrMemory Memory;
 
-#undef ZeroMemory
-#undef CopyMemory
-#undef FillMemory
-#define ZeroMemory(a,b) Memory.mem_fill(a,0,b)
-#define CopyMemory(a,b,c) memcpy(a,b,c) //. CopyMemory(a,b,c)
-#define FillMemory(a,b,c) Memory.mem_fill(a,c,b)
-
 // delete
 #ifdef __BORLANDC__
 #include "xrMemory_subst_borland.h"
