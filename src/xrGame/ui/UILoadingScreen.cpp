1837264c7ec1a50974f5160b8e8b6dd6b878d8e0
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-04-08 15:03:13 +0300

    Уже до загрузки текстур доходит

--- src/xrGame/ui/UILoadingScreen.cpp
+++ src/xrGame/ui/UILoadingScreen.cpp
@@ -20,7 +20,8 @@
 extern ENGINE_API int ps_rs_loading_stages;
 
 UILoadingScreen::UILoadingScreen()
-    : loadingLogo(nullptr), loadingProgress(nullptr),
+    : loadingLogo(nullptr), loadingProgress(nullptr), 
+      loadingProgressPercent(nullptr), loadingProgressBackground(nullptr),
       loadingStage(nullptr), loadingHeader(nullptr),
       loadingTipNumber(nullptr), loadingTip(nullptr)
 {
@@ -31,14 +32,26 @@ void UILoadingScreen::Initialize()
 {
     CUIXml uiXml;
     uiXml.Load(CONFIG_PATH, UI_PATH, "ui_mm_loading_screen.xml");
+
+    if (uiXml.NavigateToNode("loading_progress_background"))
         loadingProgressBackground = UIHelper::CreateStatic(uiXml, "loading_progress_background", this);
+
         loadingProgress = UIHelper::CreateProgressBar(uiXml, "loading_progress", this);
         CUIXmlInit::InitWindow(uiXml, "background", 0, this);
         loadingLogo = UIHelper::CreateStatic(uiXml, "loading_logo", this);
+
+    if (uiXml.NavigateToNode("loading_process_percent"))
         loadingProgressPercent = UIHelper::CreateStatic(uiXml, "loading_progress_percent", this);
+
         loadingStage = UIHelper::CreateStatic(uiXml, "loading_stage", this);
+
+    if (uiXml.NavigateToNode("loading_header"))
         loadingHeader = UIHelper::CreateStatic(uiXml, "loading_header", this);
+
+    if (uiXml.NavigateToNode("loading_tip_number"))
         loadingTipNumber = UIHelper::CreateStatic(uiXml, "loading_tip_number", this);
+
+    if (uiXml.NavigateToNode("loading_tip"))
         loadingTip = UIHelper::CreateStatic(uiXml, "loading_tip", this);
 }
 
@@ -48,7 +61,7 @@ void UILoadingScreen::Update(const int stagesCompleted, const int stagesTotal)
     if (loadingProgress->GetProgressPos() < progress)
         loadingProgress->SetProgressPos(progress);
 
-    if (ps_rs_loading_stages)
+    if (ps_rs_loading_stages && loadingProgressPercent)
     {
         char buf[5];
         xr_sprintf(buf, "%.0f%%", loadingProgress->GetProgressPos());
@@ -76,7 +89,10 @@ void UILoadingScreen::SetStageTitle(const char* title) const
 
 void UILoadingScreen::SetStageTip(const char* header, const char* tipNumber, const char* tip) const
 {
-    loadingHeader->TextItemControl()->SetText(header);
-    loadingTipNumber->TextItemControl()->SetText(tipNumber);
-    loadingTip->TextItemControl()->SetText(tip);
+    if (loadingHeader)
+        loadingHeader->TextItemControl()->SetText(header);
+    if (loadingTipNumber)
+        loadingTipNumber->TextItemControl()->SetText(tipNumber);
+    if (loadingTip)
+        loadingTip->TextItemControl()->SetText(tip);
 }
